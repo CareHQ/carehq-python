ed90ea67f17c9afc3c00f0acebd647c7faf0617e
Anthony <ant@getme.co.uk>
2025-12-28 21:59:35 +0000

    fix: remove the werkzeug dependency

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     # Versions should comply with PEP440. For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.1',
+    version='0.1.2',
     description='CareHQ API Client for Python.',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -81,8 +81,7 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-        'requests>=2.22.0',
-        'werkzeug>=2.0.2'
+        'requests>=2.22.0'
     ],
 
     # List additional groups of dependencies here (e.g. development
